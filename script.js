f178291f0cbdecffad33e6abb465cb6b7a9b91ae
Pavel <you@example.com>
2023-02-27 17:38:14 +0300

    feat: add empty cells (17:38 27.02.23)

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ body.querySelector('header').insertAdjacentHTML('beforeend', `<h1>Minesweeper</h
 body.insertAdjacentHTML('beforeend', `<section></section>`)
 const section = body.querySelector('section')
 
+section.insertAdjacentHTML('beforeend', '<div class="wrapper"></div>')
+section.querySelector('.wrapper').insertAdjacentHTML('beforeend', '<div class="field"></div>')
+const field = section.querySelector('.field')
+
+for (let i = 1; i < 17; i++) {
+    for (let j = 1; j < 17; j++) {
+        field.insertAdjacentHTML('beforeend', `<div class="cell ${i}-${j}"></div>`)
+    }
+}
+
 body.insertAdjacentHTML('beforeend', '<footer><ul><li><a href="http://github.com/PaHaNchickT">GitHub</a></li><li><a href="http://ternopavel.ru">Made by Pavel Terno</a></li><li>2023</li></ul></footer>')
 
 console.log(spTime, spEmts, spNumb, spBase)
