ce3ffa97f11f24495bd8c57e67ae8e94cdb16add
Pavel <you@example.com>
2023-02-27 19:33:18 +0300

    feat: make bomb generation engine (19:33 27.02.23)

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ import spEmts from '../../assets/modules/sprite-emotions.js'
 import spNumb from '../../assets/modules/sprite-numbers.js'
 import spBase from '../../assets/modules/sprite-base.js'
 
+//////////////////////////////////////////////////making page////////////////////////////////////////////
+
+let isStart = 0
+
 
 const body = document.querySelector('body')
 body.insertAdjacentHTML('beforeend', '<header>')
@@ -21,10 +25,39 @@ for (let i = 1; i < 17; i++) {
         field.insertAdjacentHTML('beforeend', `<div class="cell ${i}-${j}"></div>`)
     }
 }
+const cells = field.querySelectorAll('.cell')
+
+// console.log(cells)
 
 body.insertAdjacentHTML('beforeend', '<footer><ul><li><a href="http://github.com/PaHaNchickT">GitHub</a></li><li><a href="http://ternopavel.ru">Made by Pavel Terno</a></li><li>2023</li></ul></footer>')
 
-console.log(spTime, spEmts, spNumb, spBase)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////engine/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////bomb generation/////////////////////////////////////////////////
+
+function bombGen(jopa) {
+    for (field.querySelectorAll('.bomb').length; field.querySelectorAll('.bomb').length < 40;) {
+        cells.forEach(e => {
+            if ((`${Math.floor(Math.random() * 16)+1}-${Math.floor(Math.random() * 16)+1}` === e.classList[1]) && (e.classList[2] !== 'bomb')) {
+                e.classList.add('bomb')
+            }
+        })
+    }
+}
+
+//////////////////////////////////////////////////////////onclick func///////////////////////////////////////////////
+
+field.addEventListener('click', function(event) {
+    if (isStart === 0) {
+        bombGen(event.target.className)
+        isStart = 1
+        console.log(field.querySelectorAll('.bomb').length) 
+    } else {
+
+    }
+})
 
 
 
