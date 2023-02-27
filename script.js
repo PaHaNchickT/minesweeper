fdc59e4ea1c67c436c93e7ab68f0147ef1075a6c
Pavel <you@example.com>
2023-02-27 22:40:20 +0300

    feat: add bomb counter func (22:40 27.02.23)

--- script.js
+++ script.js
@@ -37,25 +37,86 @@ body.insertAdjacentHTML('beforeend', '<footer><ul><li><a href="http://github.com
 
 /////////////////////////////////////////////////////bomb generation/////////////////////////////////////////////////
 
-function bombGen(jopa) {
+function bombGen(selected) {
     for (field.querySelectorAll('.bomb').length; field.querySelectorAll('.bomb').length < 40;) {
         cells.forEach(e => {
-            if ((`${Math.floor(Math.random() * 16)+1}-${Math.floor(Math.random() * 16)+1}` === e.classList[1]) && (e.classList[2] !== 'bomb')) {
+            if ((`${Math.floor(Math.random() * 16) + 1}-${Math.floor(Math.random() * 16) + 1}` === e.classList[1]) && (e.classList[2] !== 'bomb') && (e.classList[1] !== selected)) {
                 e.classList.add('bomb')
             }
         })
     }
 }
 
+/////////////////////////////////////////////////////bomb counter/////////////////////////////////////////////////////
+
+function bombCounter(event) {
+    let count = event.classList[1].split('-'),
+        pos1 = (+count[0] - 2) * 16 + +count[1] - 2,
+        pos2 = (+count[0] - 2) * 16 + +count[1] - 1,
+        pos3 = (+count[0] - 2) * 16 + +count[1],
+        pos4 = (+count[0] - 1) * 16 + +count[1] - 2,
+        pos5 = (+count[0] - 1) * 16 + +count[1] - 1,
+        pos6 = (+count[0] - 1) * 16 + +count[1],
+        pos7 = (+count[0]) * 16 + +count[1] - 2,
+        pos8 = (+count[0]) * 16 + +count[1] - 1,
+        pos9 = (+count[0]) * 16 + +count[1],
+        pos = [],
+        bombs = 0
+
+    if (+count[1] === 1 && +count[0] === 1) {
+        pos.push(pos6, pos8, pos9)
+    } else if (+count[1] === 16 && +count[0] === 1) {
+        pos.push(pos4, pos7, pos8)
+    } else if (+count[1] === 16 && +count[0] === 16) {
+        pos.push(pos1, pos2, pos4)
+    } else if (+count[1] === 1 && +count[0] === 16) {
+        pos.push(pos2, pos3, pos6)
+    } else if (+count[1] === 1 && (+count[0] !== 1 || +count[0] !== 16)) {
+        pos.push(pos2, pos3, pos6, pos8, pos9)
+    } else if (+count[0] === 1 && (+count[1] !== 1 || +count[1] !== 16)) {
+        pos.push(pos4, pos6, pos7, pos8, pos9)
+    } else if (+count[1] === 16 && (+count[0] !== 1 || +count[0] !== 16)) {
+        pos.push(pos1, pos2, pos4, pos7, pos8)
+    } else if (+count[0] === 16 && (+count[1] !== 1 || +count[1] !== 16)) {
+        pos.push(pos1, pos2, pos3, pos4, pos6)
+    } else {
+        pos.push(pos1, pos2, pos3, pos4, pos6, pos7, pos8, pos9)
+    }
+
+    pos.forEach(e => {
+        if (cells[e].classList[2] === 'bomb') {
+            bombs++
+        }
+    })
+
+    if (event.classList[2] !== 'bomb') {
+        event.classList.add(`b${bombs}`)
+    }
+    
+}
+
+function bombsNumber() {
+    cells.forEach(el => {
+        bombCounter(el)
+    })
+}
+
+//////////
+
+function radius() {
+
+}
+
 //////////////////////////////////////////////////////////onclick func///////////////////////////////////////////////
 
-field.addEventListener('click', function(event) {
+field.addEventListener('click', function (event) {
     if (isStart === 0) {
-        bombGen(event.target.className)
         isStart = 1
-        console.log(field.querySelectorAll('.bomb').length) 
+        bombGen(event.target.classList[1])
+        bombCounter(event.target)
+        bombsNumber()
     } else {
-
+        bombCounter(event.target)
     }
 })
 
