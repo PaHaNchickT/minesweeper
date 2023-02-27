a2617ed441ade2c61c94e61c759863ceedea3980
Pavel <you@example.com>
2023-02-27 16:52:52 +0300

    feat: add sprite modules (16:52 27.02.23)

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+import spTime from '../../assets/modules/sprite-timer.js'
+import spEmts from '../../assets/modules/sprite-emotions.js'
+import spNumb from '../../assets/modules/sprite-numbers.js'
+import spBase from '../../assets/modules/sprite-base.js'
+
+
 const body = document.querySelector('body')
 body.insertAdjacentHTML('beforeend', '<header>')
 body.querySelector('header').insertAdjacentHTML('beforeend', `<h1>Minesweeper</h1>`)
@@ -5,4 +11,9 @@ body.querySelector('header').insertAdjacentHTML('beforeend', `<h1>Minesweeper</h
 body.insertAdjacentHTML('beforeend', `<section></section>`)
 const section = body.querySelector('section')
 
-body.insertAdjacentHTML('beforeend', '<footer><ul><li class="github"><a href="http://github.com/PaHaNchickT">GitHub</a></li><li class="by">Made by Pavel Terno</li><li>2023</li></ul></footer>')
+body.insertAdjacentHTML('beforeend', '<footer><ul><li><a href="http://github.com/PaHaNchickT">GitHub</a></li><li><a href="http://ternopavel.ru">Made by Pavel Terno</a></li><li>2023</li></ul></footer>')
+
+console.log(spTime, spEmts, spNumb, spBase)
+
+
+
