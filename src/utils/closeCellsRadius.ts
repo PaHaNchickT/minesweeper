06691c6adaa4e055799f38acf4b80b7c88c384d1
Pavel <you@example.com>
2024-10-08 01:11:48 +0300

    feat: fully implement cells opening engine

--- src/utils/closeCellsRadius.ts
+++ src/utils/closeCellsRadius.ts
@@ -8,25 +8,27 @@ export const closeCellsRadius = (
 ): { x: number; y: number }[] => {
   const queueCells: { x: number; y: number }[] = [];
   const viewedCells = new Set();
+
   const output: { x: number; y: number }[] = [];
 
   const zeroCellsOpening = (x: number, y: number): void => {
     queueCells.shift();
 
-    for (let countY = y - 1 < 0 ? 0 : y - 1; countY <= (y + 1 === FIELD_CONFIG.fieldSize ? y : y + 1); countY++) {
-      for (let countX = x - 1 < 0 ? 0 : x - 1; countX <= (x + 1 === FIELD_CONFIG.fieldSize ? x : x + 1); countX++) {
-        if (Array.from(viewedCells).includes(JSON.stringify({ x: countX, y: countY }))) continue;
+    if (fieldItems[y][x].innerText === 0) {
+      for (let countY = y - 1 < 0 ? 0 : y - 1; countY <= (y + 1 === FIELD_CONFIG.fieldSize ? y : y + 1); countY++) {
+        for (let countX = x - 1 < 0 ? 0 : x - 1; countX <= (x + 1 === FIELD_CONFIG.fieldSize ? x : x + 1); countX++) {
+          if (Array.from(viewedCells).includes(JSON.stringify({ x: countX, y: countY }))) continue;
 
-        output.push({ x: countX, y: countY });
+          output.push({ x: countX, y: countY });
+          viewedCells.add(JSON.stringify({ x: countX, y: countY }));
 
-        viewedCells.add(JSON.stringify({ x: countX, y: countY }));
-
-        if (
-          !(countX === x && countY === y) &&
-          fieldItems[countY][countX].innerText === 0 &&
-          !queueCells.map((item) => JSON.stringify(item)).includes(JSON.stringify({ x: countX, y: countY }))
-        ) {
-          queueCells.push({ x: countX, y: countY });
+          if (
+            !(countX === x && countY === y) &&
+            fieldItems[countY][countX].innerText === 0 &&
+            !queueCells.map((item) => JSON.stringify(item)).includes(JSON.stringify({ x: countX, y: countY }))
+          ) {
+            queueCells.push({ x: countX, y: countY });
+          }
         }
       }
     }
