741cc3a6c4d6b356ef646503ef5f5a2b3f5469aa
Pavel <you@example.com>
2024-10-07 20:33:24 +0300

    fix: change bomb radius generation for initial cell

--- src/utils/bombsGen.ts
+++ src/utils/bombsGen.ts
@@ -4,12 +4,23 @@ import { randomizer } from './randomizer';
 
 export const bombsGen = (initialCell: { x: number; y: number }): { x: number; y: number }[] => {
   const bombsPos: { x: number; y: number }[] = [];
+  const initialArray = [
+    { x: initialCell.x - 1, y: initialCell.y - 1 },
+    { x: initialCell.x, y: initialCell.y - 1 },
+    { x: initialCell.x + 1, y: initialCell.y - 1 },
+    { x: initialCell.x - 1, y: initialCell.y },
+    { x: initialCell.x, y: initialCell.y },
+    { x: initialCell.x + 1, y: initialCell.y },
+    { x: initialCell.x - 1, y: initialCell.y + 1 },
+    { x: initialCell.x, y: initialCell.y + 1 },
+    { x: initialCell.x + 1, y: initialCell.y + 1 },
+  ];
 
   const posSelection = (): void => {
     const currentPosition = { x: randomizer(FIELD_CONFIG.fieldSize), y: randomizer(FIELD_CONFIG.fieldSize) };
 
     if (
-      JSON.stringify(currentPosition) === JSON.stringify(initialCell) ||
+      initialArray.map((pos) => JSON.stringify(pos)).includes(JSON.stringify(currentPosition)) ||
       bombsPos.map((pos) => JSON.stringify(pos)).includes(JSON.stringify(currentPosition))
     ) {
       posSelection();
