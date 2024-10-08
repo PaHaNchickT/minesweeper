69908ab05b03e3a757f901f318be8024b6dfb667
Pavel <you@example.com>
2024-10-08 14:10:40 +0300

    feat: turn off contextmenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { type ReactElement } from 'react';
+import { useEffect, type ReactElement } from 'react';
 import { Provider } from 'react-redux';
 
 import ControlPanel from '@/components/ControlPanel/ControlPanel';
@@ -9,6 +9,10 @@ import Field from '@/components/Field/Field';
 import { store } from '../redux/store';
 
 const App = (): ReactElement => {
+  useEffect(() => {
+    document.addEventListener('contextmenu', (event) => event.preventDefault());
+  }, []);
+
   return (
     <div className="w-screen h-screen flex flex-col gap-5 justify-center items-center">
       <Provider store={store}>
