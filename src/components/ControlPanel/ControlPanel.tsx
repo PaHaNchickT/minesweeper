86c8cc691fe0a5c7d817100d914610d28c52b590
Pavel <you@example.com>
2024-10-09 23:43:38 +0300

    feat: implement styles for smile

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -16,6 +16,9 @@ import ControlPanelItems from '../ControlPanelItems/ControlPanelItems';
 const ControlPanel = (): ReactElement => {
   const dispatch = useDispatch();
   const [seconds, setSeconds] = useState(0);
+  const [smileStyles, setSmileStyles] = useState(
+    'border-4 border-l-white border-t-white border-r-[#808080] border-b-[#808080]',
+  );
   const gameState = useSelector((state: RootState) => state.gameState);
 
   // Timer
@@ -31,18 +34,28 @@ const ControlPanel = (): ReactElement => {
   return (
     <div className="w-full p-2 flex justify-between items-center box-border border-4 border-l-[#808080] border-t-[#808080] border-r-white border-b-white">
       <ControlPanelItems innerText={(FIELD_CONFIG.bombsCount - gameState.flagsCount).toString().padStart(3, '0')} />
-      <Button
-        onPress={() => {
-          setSeconds(0);
-          dispatch(clearField());
-          dispatch(generationOff());
-          dispatch(clearGame());
-          dispatch(clearFlags());
-          dispatch(updateClickStatus('🙂'));
-        }}
-      >
-        {gameState.clickStatus}
-      </Button>
+      <div className="relative">
+        <Button
+          className={`bg-[#c0c0c0] relative z-[1] text-2xl p-0 flex leading-[1.2] flex-col justify-start min-w-0 w-[43px] h-[43px] rounded-none ${smileStyles}`}
+          onPress={() => {
+            setSeconds(0);
+            dispatch(clearField());
+            dispatch(generationOff());
+            dispatch(clearGame());
+            dispatch(clearFlags());
+            dispatch(updateClickStatus('🙂'));
+          }}
+          onPressStart={() =>
+            setSmileStyles('border-4 border-l-[#808080] border-t-[#808080] border-r-[#c0c0c0] border-b-[#c0c0c0]')
+          }
+          onPressEnd={() =>
+            setSmileStyles('border-4 border-l-white border-t-white border-r-[#808080] border-b-[#808080]')
+          }
+        >
+          {gameState.clickStatus}
+        </Button>
+        <div className="w-[47px] h-[47px] bg-[#808080] absolute left-[-2px] top-[-2px] z-0"></div>
+      </div>
       <ControlPanelItems innerText={seconds.toString().padStart(3, '0')} />
     </div>
   );
